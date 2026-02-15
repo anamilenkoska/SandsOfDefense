b4ca07e6fb32b144f4c6b10571c7848e9d4ba292
Ana <89231022@student.upr.si>
2026-02-15 15:53:54 +0100

    Preanimation to enemy

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public Color glowColor;
+    public float intensity=8f;
+    private Material _material;
+
+    void Start()
+    {
+        _material=GetComponent<Renderer>().material;
+        _material.EnableKeyword("_EMISSION");
+        _material.SetColor("_EmissionColor",glowColor.linear*intensity);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
